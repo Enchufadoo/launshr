1134391291e2952636cc579bf51177673e8374e4
Enchufadoo <1263356+Enchufadoo@users.noreply.github.com>
2022-06-17 17:44:20 -0300

    Add the description of a command as  component

---
+++ views/commandlist/description_test.go
@@ -0,0 +1,40 @@
+package commandlist
+
+import (
+	"github.com/stretchr/testify/assert"
+	"launshr/parser"
+	"testing"
+)
+
+func TestRenderDescription(t *testing.T) {
+	cases := []struct {
+		description string
+		node        parser.CommandNode
+		want        string
+	}{
+		{
+			"Getting a description with full node data",
+			parser.CommandNode{Name: "Hola", Command: "cd hola", WorkingDirectory: "/"},
+			"Hola\ncd hola\nWorking Directory: /",
+		},
+		{
+			"Parent node gets an empty description",
+			parser.CommandNode{Name: "Hola", Parent: &parser.CommandNode{}},
+			"",
+		},
+		{
+			"No name and working directory just shows the command",
+			parser.CommandNode{Command: "cd hi"},
+			"cd hi\n",
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.description, func(t *testing.T) {
+			got := RenderDescription(test.node)
+
+			assert.Equal(t, test.want, got)
+		})
+	}
+
+}
